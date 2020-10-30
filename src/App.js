79ab8d4855c362cca26f555aa84fcaaa53252678
kazalghosh <mrgkazal@gmail.com>
2020-10-30 21:44:08 +0600

    home page

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+        <Route exact path="/">
+            <Home></Home>
+          </Route>
+          
           <Route path="/home">
             <Home></Home>
           </Route>
